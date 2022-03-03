0c919f992947d647bf3833e63dc6fdcec386de5f
EthanN <newberrydethan@gmail.com>
2022-03-02 22:13:12 -0600

    Added red error border in javascript and moved error message to under password from confirm in HTML

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ var check = function () {
   if (document.getElementById('password').value ==
     document.getElementById('confirm').value) {
     document.getElementById('message').innerHTML = '';
+
+    document.getElementById('password').style.border = '1px solid rgba(0, 0, 0, .3)'
+    document.getElementById('confirm').style.border = '1px solid rgba(0, 0, 0, .3)'
   } else {
     document.getElementById('message').style.color = 'red';
     document.getElementById('message').innerHTML = '* Pass does not match';
+
+    document.getElementById('password').style.border = '1px solid red'
+    document.getElementById('confirm').style.border = '1px solid red'
   }
 }
